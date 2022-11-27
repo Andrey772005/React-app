4cdaaa10d7c88d41dbbda44a9b42f93e48212fdc
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2022-11-27 21:35:52 +0300

    Create components 27.11.2022

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../ui/logo/Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Logo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent } from "../../../assets/logo.svg";
+
+function Logo() {
+	return (
+		<a href="/">
+			<ReactComponent />
+		</a>
+	)
+}
+
+export default Logo;
+
