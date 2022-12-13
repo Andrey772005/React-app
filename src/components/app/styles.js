92b9bc633edeccc239ed92b8e46e0d4398d8220d
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2022-12-13 21:32:03 +0300

    Create Header 13.12.2022

---
+++ src/components/app/styles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+	min-height: 100%;
+  	margin-left: calc(100vw - 100%)
+}
+
+body,
+html {
+	margin: 0;
+}
+
+body {
+	position: relative;
+	min-height: 100%;
+  margin-left: calc(100vw - 100%);
+  font-family: ${(props) => props.theme.fontFamily};
+	font-style: normal;
+	font-weight: 400;
+	font-size: ${(props) => props.theme.fontSizeDefault};
+	line-height: 1.5;
+	color: ${(props) => props.theme.textColor};
+}
+`;
--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,4 +1,7 @@
 export const theme = {
   colorWhite: '#ffffff',
   backgroundColor: '#7859cfe6',
+  fontFamily: '"Inter", "Arial", sans-serif',
+  fontSizeDefault: '18px',
+  textColor: '#333333',
 }
