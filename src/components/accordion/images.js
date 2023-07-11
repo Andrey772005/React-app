31024c5f8d1027bcc2a7a2f39468e35d52100725
Andrey772005 <45210596+Andrey772005@users.noreply.github.com>
2023-07-11 23:05:02 +0300

    Accordion logic 11.07.2023

--- src/components/accordion/images.js
+++ src/components/accordion/images.js
@@ -1,28 +1,29 @@
-import { Image } from "../../assets/1.webp";
+import Image  from "../../assets/1.webp";
+import Image2 from "../../assets/2.webp";
 
 export const images = [
     {
-        image: {Image},
+        image: Image,
+        header: "Canada",
+        text: "Image description"
+    },
+    {
+        image: Image2,
         header: "",
         text: ""
     },
     {
-        image: {Image},
+        // image: {Image},
         header: "",
         text: ""
     },
     {
-        image: {Image},
+        // image: {Image},
         header: "",
         text: ""
     },
     {
-        image: {Image},
-        header: "",
-        text: ""
-    },
-    {
-        image: {Image},
+        // image: {Image},
         header: "",
         text: ""
     }
--- src/components/blocks/main/Main.jsx
+++ src/components/blocks/main/Main.jsx
@@ -4,6 +4,7 @@ import Main from "../../../assets/main.webp";
 import {Img} from "../../../styled/img/Image";
 import {Figure} from "../../../styled/figure/Figure";
 import SwiperSlider from "../../swiper/Swiper";
+// import {Accordion} from "../../accordion/Accordion";
 
 
 function PageWrapper() {
@@ -16,6 +17,7 @@ function PageWrapper() {
           каждому из вас обрести главного человека в своей жизни &ndash; себя&#x00021;
         </figcaption>
       </Figure>
+      {/*<Accordion/>*/}
       <SwiperSlider/>
     </MainPage>
 
