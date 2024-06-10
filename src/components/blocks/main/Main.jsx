774388ec9edd2845fafdb20bdd461d566b3c8f3d
Andrew <as772005@gmail.com>
2024-06-10 21:10:06 +0300

    rebuild the project

--- src/components/blocks/main/Main.jsx
+++ src/components/blocks/main/Main.jsx
@@ -9,11 +9,6 @@ function PageWrapper() {
   return (
     <MainPage>
       <Figure>
-        <Img src={Main}></Img>
-        <figcaption>
-          Каждый человек в этом мире ищет главное &ndash; счастье&#x0002C; любовь и гармонию с самим собой&#x0002E; Наша задача помочь
-          каждому из вас обрести главного человека в своей жизни &ndash; себя&#x00021;
-        </figcaption>
       </Figure>
       <SwiperSlider/>
     </MainPage>
--- src/components/swiper/Swiper.jsx
+++ src/components/swiper/Swiper.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import Foto1 from "../../assets/1.webp";
-import Foto2 from "../../assets/2.webp";
 
 import "./styles.css";
 
@@ -15,10 +13,10 @@ export default function SwiperSlider() {
     <>
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
-          <img src={Foto1} alt="foto1"></img>
+         
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Foto2} alt="foto2"></img>
+          
         </SwiperSlide>
       </Swiper>
     </>
